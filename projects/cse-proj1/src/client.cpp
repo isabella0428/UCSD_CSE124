cff53d8c3fb5542bab4b7bf645706e217fa08f3e
isabella <isabella_aus_china@163.com>
2020-02-03 11:53:28 +0800

    Modify client.cpp

--- projects/cse-proj1/src/client.cpp
+++ projects/cse-proj1/src/client.cpp
@@ -18,7 +18,8 @@ Date:   2020.1.28
 
 #include "client.hpp"
 
-int Client::connect_to_server(int port_num) {
+int Client::connect_to_server(int port_num)
+{
     std::cout << "Client side" << std::endl;
 
     // Create a socket
@@ -57,7 +58,8 @@ int Client::connect_to_server(int port_num) {
     return socket_id;
 }
 
-void Client::sendRequest(char *message, int socket_id) {
+void Client::sendRequest(char *message, int socket_id)
+{
     if (send(socket_id, message, sizeof(message) + 100, 0) == 0)
     {
         perror("Send Connction Request Failed!");
@@ -89,38 +91,42 @@ void Client::receive(int socket_id, std::string path)
         fullResponse.resize(totalLength);
         memcpy(&fullResponse[lastSize], respond, len);
     }
-    std::string response(&fullResponse[0], totalLength);
+    std::string received(&fullResponse[0], totalLength);
+
     // Find filename
-    int idx = response.find("Filename: ");
+    int idx = received.find("Filename: ");
     if (idx == (int)std::string::npos)
     {
         return;
     }
-    int fileContentId = response.find("\r\n\r\n");
+    int fileContentId = received.find("\r\n\r\n");
     if (fileContentId == (int)std::string::npos)
     {
         return;
     }
-    std::string filename = response.substr(idx + 10, response.find("Last") - idx - 12);
-    std::string file = response.substr(fileContentId + 4, response.size() - fileContentId - 4);
+    std::string response = received.substr(0, fileContentId + 4);
+    std::cout << response;
+    std::string filename = received.substr(idx + 10, received.find("Last") - idx - 12);
+    std::string file = received.substr(fileContentId + 4, response.size() - fileContentId - 4);
     saveFile(filename, file, path);
 }
 
-void Client::saveFile(std::string filename, std::string file, std::string path) {
+void Client::saveFile(std::string filename, std::string file, std::string path)
+{
     chdir((char *)path.c_str());
     std::ofstream f;
     char absolute_path[300];
     realpath(filename.c_str(), absolute_path);
     f.open(absolute_path);
 
-    if (f.is_open()) {
+    if (f.is_open())
+    {
         f.write(file.c_str(), file.size());
         f.close();
-        std::cout<<"File saved to: "<<absolute_path<<std::endl;
-    } else {
-        std::cout<<("Failed to Save file")<<std::endl;
+        std::cout << "File saved to: " << absolute_path << std::endl;
+    }
+    else
+    {
+        std::cout << ("Failed to Save file") << std::endl;
     }
 }
-
-
-
